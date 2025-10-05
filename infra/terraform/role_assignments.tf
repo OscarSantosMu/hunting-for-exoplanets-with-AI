9a043c13eb987603b4ca36fda3a984cf856285f1
Oscar Santos <oscarsantosmu@gmail.com>
2025-10-05 02:12:44 -0600

    ci: rm dup role

--- infra/terraform/role_assignments.tf
+++ infra/terraform/role_assignments.tf
@@ -4,10 +4,3 @@ resource "azurerm_role_assignment" "kv_secrets_user_admin" {
   role_definition_name = "Key Vault Secrets User"
   principal_id         = data.azuread_service_principal.tfstate.object_id  # or a user id
 }
-
-# Example: your ML workspace or app identity, if it needs secrets read:
-resource "azurerm_role_assignment" "kv_secrets_user_ml" {
-  scope                = azurerm_key_vault.cosmo.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = data.azuread_service_principal.ml_workspace.object_id
-}
