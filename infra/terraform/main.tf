d6c04ceb067735f04840dd6b7f82636692f239f3
Oscar Santos <oscarsantosmu@gmail.com>
2025-10-04 19:04:35 -0600

    ci: fix supported region

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -124,7 +124,7 @@ resource "azurerm_machine_learning_workspace" "exo" {
 
 resource "azurerm_static_web_app" "exo" {
   name                = var.static_web_app_name
-  location            = azurerm_resource_group.exo.location
+  location            = "${azurerm_resource_group.exo.location}2"
   resource_group_name = azurerm_resource_group.exo.name
   sku_tier            = var.static_web_app_sku_tier
   sku_size            = var.static_web_app_sku_size
